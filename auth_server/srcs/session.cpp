d7ca0530912c195887c921035a610f2ce9cd5989
BenYoo-ben <neverknown0909@gmail.com>
2021-12-19 19:12:58 +0900

    feat: sms auth test
    tested TCP connection with sms client(android tcp)
    
    [BenYoo - 121219]

--- auth_server/srcs/session.cpp
+++ auth_server/srcs/session.cpp
@@ -1,10 +1,11 @@
 #include "session.hpp"
 #include "global.hpp"
+#include "random_generator.hpp"
 
 template<class C, void* (C::* thread_run)()>
 void* pthread_member_wrapper(void* data) {
-  C* obj = static_cast<C*>(data);
-  return (obj->*thread_run)();
+	C* obj = static_cast<C*>(data);
+	return (obj->*thread_run)();
 }
 
 session_object::session_object(int established_socket){
@@ -18,19 +19,23 @@ void *session_object::run(){
 	char buffer[global_expected_MTU];
 	int bytes_read;
 
-	while(true){
+	random_number_generator rng;
 
-		memset(buffer,0x0,global_expected_MTU);
+	int sms_data = rng.generate_int(6);
 
-		bytes_read = read(c_sock,buffer,global_expected_MTU);
-		if(bytes_read < 1){
-			std::cerr << "Invalid Read" << std::endl;
-			close_socket();
-			break;
-		}
-		else{
-			
-		}
+	memset(buffer,0x0,global_expected_MTU);
+	sprintf(buffer, "%d", sms_data);
+	write(c_sock, buffer,global_expected_MTU);
+
+	memset(buffer,0x0,global_expected_MTU);
+	bytes_read = read(c_sock,buffer,global_expected_MTU);
+	if(bytes_read < 1){
+		std::cerr << "Invalid Read" << std::endl;
+		close_socket();
+	}
+	else{
+		printf("Auth complete ! : RECV=%d\n",buffer);
+
 	}
 	return NULL;
 }
